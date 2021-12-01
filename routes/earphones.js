310e534dde792ce01825cc77556183b6e5fade8e
919609183 <89273912+919609183@users.noreply.github.com>
2021-11-30 22:43:48 -0600

    Update earphones.js

--- routes/earphones.js
+++ routes/earphones.js
@@ -1,33 +1,24 @@
 var express = require('express');
 const earphones_controlers= require('../controllers/earphones');
 var router = express.Router();
-
-
-
+const secured = (req, res, next) => {
+  if (req.user){
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+}
 /* GET earphones */
 router.get('/', earphones_controlers.earphones_view_all_Page );
-module.exports = router;
-
-
-
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-res.render('earphones', { title: 'Search Results Ear Phones' });
-});
-
-// GET request for one costume.
-router.get('/earphones/:id', earphones_controlers.earphones_detail);
-
-/* GET detail costume page */
+/* GET detail earphones page */
 router.get('/detail', earphones_controlers.earphones_view_one_Page);
-
-/* GET create costume page */
-router.get('/create', earphones_controlers.earphones_create_Page);
-
+/* GET create earphones page */
+router.get('/create', secured, earphones_controlers.earphones_create_Page);
 /* GET create update page */
-router.get('/update', earphones_controlers.earphones_update_Page);
+// router.get('/update', earphones_controlers.earphones_update_Page);
+/* GET update earphones page */
+router.get('/update', secured, earphones_controlers.earphones_update_Page);
+/* GET create earphones page */
+router.get('/delete', secured, earphones_controlers.earphones_delete_Page);
 
-/* GET create costume page */
-router.get('/delete', earphones_controlers.earphones_delete_Page);
 module.exports = router;
